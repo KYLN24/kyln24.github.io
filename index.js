c4929f4683ef683e2b4bbb49fefd55e5b13e926a
Kylin24Kwok <1296845690@qq.com>
2023-02-21 16:03:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,24 +11,6 @@ const app = new Vue({
         mainNav: [
             {
                 id: 1,
-                title: "Project",
-                subTitle: "一些有趣的小玩意儿～",
-                link: "/project",
-            },
-            {
-                id: 2,
-                title: "learnSpace",
-                subTitle: "KYLN 的技术博客",
-                link: "/learnSpace",
-            },
-            {
-                id: 3,
-                title: "Elsewhere",
-                subTitle: "KYLN 的一隅小站",
-                link: "/Elsewhere",
-            },
-            {
-                id: 4,
                 title: "GitHub",
                 subTitle: "看看我的 GitHub 首页",
                 link: "https://github.com/KYLN24",
